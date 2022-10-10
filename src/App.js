8f14deb2aabe9ebc04581f15f2573eccc672b668
FAForhad <faforhad450@gmail.com>
2022-10-10 21:40:55 +0600

    added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!!!!!!!!!!!!!!!!!!!!!</div>,
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
