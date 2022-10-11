4294cd817b12529c13e9c9164dccaff536feb3b3
FAForhad <faforhad450@gmail.com>
2022-10-11 18:04:30 +0600

    quiz-answer-set

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const Options = ({ option, correctAnswer }) => {
     console.log(correctAnswer)
-
+    const correctQuiz = event => {
+        const value = event.target.innerText;
+        if (value === correctAnswer) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Correct...',
+                text: `${correctAnswer}`,
+            })
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Wrong Answer!!!',
+            })
+        }
+    }
     return (
         <div>
-            <div className='p-5 border border-teal-600 mx-2 my-2 flex justify-between hover:bg-teal-900'>
+            <div onClick={(event) => correctQuiz(event)} className='p-5 border border-teal-600 mx-2 my-2 flex justify-between hover:bg-teal-900'>
                 {option}
             </div>
         </div>
