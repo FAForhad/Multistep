9f7b1e379fd6cd65a76ce879d20acd53ad98a047
FAForhad <faforhad450@gmail.com>
2022-10-11 10:54:04 +0600

    blog-3

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,8 +4,8 @@ const Blogs = () => {
     return (
         <div>
             <div className="mockup-window border border-base-300">
-                <div className="flex justify-center m-4 px-4 my-16 py-16 border-t border-base-300">
-                    <div className="card min-w-sm max-w-lg p-6 mx-4 bg-teal-700 text-primary-content">
+                <div className="md:flex lg:flex justify-center m-4 lg:px-4 lg:my-16 lg:py-24 border-t border-base-300">
+                    <div className="card min-w-sm max-w-lg p-6 my-2 mx-4 bg-teal-700 text-primary-content">
                         <div className="card-body">
                             <h2 className="card-title text-2xl">What is the porpose of react router ?</h2>
                             <p>React Router keeps your UI in sync with the URL, It has a simple API with powerful features like dynamic route matching, React Router switches from server-side to client-side, so when you click on the Link, the app checks the routes and loads the route and jump to the route</p>
@@ -13,10 +13,18 @@ const Blogs = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="card min-w-sm max-w-lg p-6 mx-4 bg-teal-700 text-primary-content">
+                    <div className="card min-w-sm max-w-lg p-6 my-2 mx-4 bg-teal-700 text-primary-content">
                         <div className="card-body">
                             <h2 className="card-title text-2xl">How dose context API works ?</h2>
                             <p>React Context API was introduced to make the state management easier in React applications,Context API is a dependency injection mechanism that allows passing data through the component tree without having to pass props down manually at every level</p>
+                            <div className="card-actions justify-end">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card min-w-sm max-w-lg p-6 my-2 mx-4 bg-teal-700 text-primary-content">
+                        <div className="card-body">
+                            <h2 className="card-title text-2xl">About useRef ?</h2>
+                            <p>useRef is a built-in React Hook.The useRef Hook in React can be used to directly access DOM nodes,useRef can be used to store local mutable value in a component.</p>
                             <div className="card-actions justify-end">
                             </div>
                         </div>
